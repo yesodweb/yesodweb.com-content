a62d8cce8f830e05ad09f98080b778f212d3a9d3
Michael Snoyman <michael@snoyman.com>
2014-01-27 16:03:02 +0200

    Flatten directory structure (matches OReilly Atlas)

--- book/generate.sh
+++ book/generate.sh
@@ -2,10 +2,13 @@
 
 rm -rf generated-xml
 mkdir generated-xml
-for f in chapters/*.asciidoc
+for f in *.asciidoc
 do
     BN=`basename $f`
     FILEID="${BN%.asciidoc}"
-    DEST="generated-xml/${FILEID}.xml"
-    asciidoc -b docbook45 --attribute=idprefix="${FILEID}_" -o "$DEST" "$f"
+    if [ "$FILEID" != "yesod-web-framework-book" -a "$FILEID" != "pr01" ]
+    then
+        DEST="generated-xml/${FILEID}.xml"
+        asciidoc -b docbook45 --attribute=idprefix="${FILEID}_" -o "$DEST" "$f"
+    fi
 done
