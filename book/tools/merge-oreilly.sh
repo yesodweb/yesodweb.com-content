2ad79709167380f68a6914458a52fcf0c02476bc
Michael Snoyman <michael@snoyman.com>
2014-10-19 12:16:30 +0300

    Tweak merge-oreilly

--- book/tools/merge-oreilly.sh
+++ book/tools/merge-oreilly.sh
@@ -4,4 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../..
 
 git subtree pull --prefix=book/asciidoc/ ~/writing/oreilly/ master          
-git subtree push --prefix=book/asciidoc/ ~/writing/oreilly/ master
+git branch -D just-asciidoc || true
+git subtree split --prefix=book/asciidoc/ -b just-asciidoc
+cd ~/writing/oreilly
+git pull $DIR/../.. just-asciidoc
