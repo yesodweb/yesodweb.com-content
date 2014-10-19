503ee5a4a471fbe6d29c8fbbb6f09fd1c7eba29e
Michael Snoyman <michael@snoyman.com>
2014-10-19 11:30:53 +0300

    tools subdir

--- book/tools/generate.sh
+++ book/tools/generate.sh
@@ -1,19 +1,20 @@
 #!/bin/bash -ex
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #rm -rf generated-xml
-mkdir -p generated-xml
-for f in asciidoc/*.asciidoc
+mkdir -p $DIR/../generated-xml
+for f in $DIR/../asciidoc/*.asciidoc
 do
     BN=`basename $f`
     FILEID="${BN%.asciidoc}"
     if [ "$FILEID" != "pr01" -a "$FILEID" != "book" ]
     then
-        DEST="generated-xml/${FILEID}.xml"
+        DEST="$DIR/../generated-xml/${FILEID}.xml"
 
         if [[ ! -f "$DEST" ]] || [[ "$f" -nt "$DEST" ]]
         then
             asciidoc -b docbook45 --attribute=idprefix="${FILEID}_" -o tmp "$f"
-            runghc strip-article-info.hs tmp
+            runghc $DIR/strip-article-info.hs tmp
             mv tmp "$DEST"
         fi
     fi
--- book/tools/test-snippets.sh
+++ book/tools/test-snippets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
-./generate.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$DIR/generate.sh
 cabal install xml-conduit classy-prelude-conduit sphinx xml-hamlet markdown yesod persistent-sqlite persistent-postgresql monadcryptorandom wai-conduit esqueleto --force-reinstalls -j --max-backjumps=-1
-runghc extract-code.hs
+runghc $DIR/extract-code.hs
 (cd extracted && ghc --make *.hs)
