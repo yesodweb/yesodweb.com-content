5b36d3de591f94bc766f7c00c4c76769b0bdb228
Michael Snoyman <michael@snoyman.com>
2014-10-19 11:27:44 +0300

    Move asciidoc files to subdir

--- book/generate.sh
+++ book/generate.sh
@@ -2,11 +2,11 @@
 
 #rm -rf generated-xml
 mkdir -p generated-xml
-for f in *.asciidoc
+for f in asciidoc/*.asciidoc
 do
     BN=`basename $f`
     FILEID="${BN%.asciidoc}"
-    if [ "$FILEID" != "yesod-web-framework-book" -a "$FILEID" != "pr01" -a "$FILEID" != "book" ]
+    if [ "$FILEID" != "pr01" -a "$FILEID" != "book" ]
     then
         DEST="generated-xml/${FILEID}.xml"
 
