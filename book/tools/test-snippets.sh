0a417d42025a73b1839b784f5c6ed61a22f08fc3
Michael Snoyman <michael@snoyman.com>
2018-04-04 10:30:35 +0300

    More fixes to test-snippets.sh

--- book/tools/test-snippets.sh
+++ book/tools/test-snippets.sh
@@ -1,7 +1,13 @@
-#!/bin/bash -ex
+#!/usr/bin/env bash
+
+set =eux
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/generate.sh
 #cabal install xml-conduit classy-prelude-conduit sphinx xml-hamlet markdown yesod persistent-sqlite persistent-postgresql monadcryptorandom wai-conduit esqueleto --force-reinstalls -j --max-backjumps=-1
-stack --resolver lts-11.3 runghc --package classy-prelude-conduit $DIR/extract-code.hs
-(cd extracted && stack --resolver lts-11.3 ghc --package classy-prelude-yesod -- --make *.hs)
+stack --resolver lts-11.3 runghc \
+      --package classy-prelude-conduit \
+      --package markdown \
+      --package sphinx \
+      $DIR/extract-code.hs
+(cd extracted && stack --resolver lts-11.3 ghc --package classy-prelude-yesod *.hs)
